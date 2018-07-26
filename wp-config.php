bdb0d8ec716cbafa34f72b6022f922ce405cb8f9
softcomet <realseniordev210@gmail.com>
2018-07-26 17:51:52 +0200

    enable debug true

--- wp-config.php
+++ wp-config.php
@@ -78,7 +78,7 @@ $table_prefix  = 'wp_';
  *
  * @link https://codex.wordpress.org/Debugging_in_WordPress
  */
-define('WP_DEBUG', false);
+define('WP_DEBUG', true);
 
 /* That's all, stop editing! Happy blogging. */
 
